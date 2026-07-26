764379764891f793633801f8f95cd74e6870bad7
Glam Chica <linternalightentertainment@gmail.com>
2026-07-26 20:35:05 +0700

    first

---
+++ DevLog.js
@@ -0,0 +1,24 @@
+const devlogData = [
+/*
+    {
+        title: "title",
+        picture: "./GFX/",
+        info: "info",
+        date: "00/00/2026"
+    },
+*/
+
+    {
+        title: "Dialogue System #2",
+        picture: "./GFX/DevLog_3.webp",
+        info: "ปรับปรุงเรื่องของ Dialogue System ให้มีการใช้งาน Enum มากขึ้น เพื่อความสะดวกในการเขียนโค้ด รวมถึงการเพิ่มความยืดหยุ่นของโค้ดมากขึ้นโดยการแบ่งประเภทของ Dialogue ออกเป็นหลากหลายประเภท เช่น SimpleChat-จะใช้ในการแสดงบทสนาทนาไปมา Trade-จะแสดงแถบไอเทมของผู้เล่นสำหรับการเลือกไอเทมที่จะขายให้NPCตัวนั้นๆ Leave-สำหรับการยกเลิกการสนทนาหรือการค้าขาย เป็นต้น",
+        date: "26/07/2026"
+    },
+    {
+        title: "Dialogue System #1",
+        picture: "./GFX/DevLog_1.webp",
+        info: "Rewrite Framework ของ Widget Dialogue ใหม่ ให้สามารถแสดงข้อความได้เองโดยดึงข้อมูล Next ID และ Delay Time จาก Data Table โดยไม่ต้องไปเขียนโค้ดซับซ้อน นอกจากการสั่ง ID ของ Dialogue ที่ต้องการให้ถูกต้อง",
+        date: "01/07/2026"
+    },
+
+];
