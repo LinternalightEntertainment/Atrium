4c1e356bf56ea11a08fb419eef9b89020c843443
Glam Chica <linternalightentertainment@gmail.com>
2026-08-24 20:54:36 +0700

    Update

---
+++ Mod_Image.js
@@ -0,0 +1,21 @@
+const Mod_Image_Data = [
+/*
+    {
+        picture: "./GFX/"
+    },
+*/
+
+    {
+        picture: "./GFX/Mod/ShinsRobotics_Employee.png"
+    },
+    {
+        picture: "./GFX/Mod/ShinsRobotics_Main.png"
+    },
+    {
+        picture: "./GFX/Mod/ShinsRobotics.png"
+    },
+    {
+        picture: "./GFX/Mod/ShinsRobotics_TraitsOccupation.png"
+    },
+
+];
