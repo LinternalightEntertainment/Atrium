45af91d9d7e8dbf597885b42c43b600538060d46
Glam Chica <linternalightentertainment@gmail.com>
2026-09-01 22:19:34 +0700

    Update

---
+++ Mod_Description.js
@@ -0,0 +1,13 @@
+const Mod_Description_Data = [
+    { picture: "./GFX/Description/FriedEgg.png" },
+    { picture: "./GFX/Description/Kapi.png" },
+    { picture: "./GFX/Description/MooKrob.png" },
+    { picture: "./GFX/Description/Moo_Ping.png" },
+    { picture: "./GFX/Description/PadKrapaoMooKrob.png" },
+    { picture: "./GFX/Description/Pad_Thai.png" },
+    { picture: "./GFX/Description/Pa_Lo.png" },
+    { picture: "./GFX/Description/Saamkler.png" },
+    { picture: "./GFX/Description/Steamed_Egg.png" },
+    { picture: "./GFX/Description/Tom Yum Goong.png" },
+    { picture: "./GFX/Description/YumMaMuang.png" }
+];
